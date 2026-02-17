4b8c210e2c0844065d02c9dd156ce1e00e3f5b63
klsnkmaria <168370929+klsnkmaria@users.noreply.github.com>
2026-02-18 01:58:20 +0200

    Add files via upload

---
+++ simulator/logger.py
@@ -0,0 +1,26 @@
+import json
+import datetime
+from pathlib import Path
+import config
+
+
+def log_event(event_name: str, payload: dict):
+    # Визначаємо корінь: папка simulator -> корінь проєкту
+    project_root = Path(__file__).resolve().parent.parent
+    log_path = project_root / config.SANDBOX_DIR / config.LOG_NAME
+
+    entry = {
+        "time": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        "event": event_name,
+        "payload": payload
+    }
+
+    logs = []
+    if log_path.exists():
+        try:
+            logs = json.loads(log_path.read_text(encoding="utf-8"))
+        except:
+            logs = []
+
+    logs.append(entry)
+    log_path.write_text(json.dumps(logs, indent=4), encoding="utf-8")
