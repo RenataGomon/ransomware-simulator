917278064cc7c485924e64359b4f4b80368b0a43
VSidd976 <siddv06@gmail.com>
2026-02-17 17:24:33 +0200

    refactoring

--- simulator/discovery.py
+++ simulator/discovery.py
@@ -3,10 +3,10 @@ import sys
 sys.path.insert(0, str(Path(__file__).parent.parent))
 import config
 
-cwd = Path(__file__)
+cwd = Path(__file__).parent
 
 def ensure_sandbox_safe():
-    if not Path.exists(cwd.parent.parent / config.SANDBOX_DIR) or not Path.exists(cwd.parent.parent / config.SANDBOX_DIR / config.MARKER_NAME):
+    if not Path.exists(cwd.parent / config.SANDBOX_DIR) or not Path.exists(cwd.parent / config.SANDBOX_DIR / config.MARKER_NAME):
         return False
     else:
         return True
@@ -14,10 +14,10 @@ def ensure_sandbox_safe():
 def list_target_files():
     target_files = []
     for i in config.ALLOWED_EXT:
-        for j in (cwd.parent.parent / config.SANDBOX_DIR).glob(f"*{i}"):
+        for j in (cwd.parent / config.SANDBOX_DIR).glob(f"*{i}"):
             target_files.append(str(Path(j)))
     return target_files
 
 if __name__ == "__main__":
-    print(f"ensure_sandbox_safe: {ensure_sandbox_safe()}")
-    print(f"list_target_files: {list_target_files()}")
+    print(f"ensure_sandbox_safe(): {ensure_sandbox_safe()}")
+    print(f"list_target_files(): {list_target_files()}")
